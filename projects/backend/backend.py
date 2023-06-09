c60cf77d54851c8edb3d719bb0b7651d9ffcfa5e
May Tusek <tusek.may@gmail.com>
2023-06-09 02:58:34 -0700

    Backend is working.

--- projects/backend/backend.py
+++ projects/backend/backend.py
@@ -1,10 +1,34 @@
+import dotenv
+import os
 from flask import Flask
+import completion
+import logging
+
+dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
 app = Flask(__name__)
+completion_controller = None
 
 @app.route('/chat/<string:prompt>')
 def chat(prompt):
-    return prompt
+  if completion is None:
+    logging.error('completions controller is not initialized')
+    return ''
+
+  prompt = prompt.strip() + " ->"
+
+  try:
+    response = completion_controller.complete(prompt)
+    return response
+  except:
+    logging.error('completion failed')
+    return "Completion failed :(", 500
 
 if __name__ == '__main__':
+  OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+  try:
+    completion_controller = completion.CompletionController(api_key=OPENAI_API_KEY)
+  except:
+    completion.abort('Unable to initialize CompletionController')
+    
   app.run()
---
+++ projects/backend/completion.py
@@ -0,0 +1,32 @@
+import openai
+import logging
+
+def abort(msg):
+  logging.error(msg)
+  exit(1)
+
+class CompletionController:
+  def __init__(self, api_key=None):
+    self.api_key = api_key
+    if self.api_key is None: abort('OPENAI_API_KEY is not set')
+    openai.api_key = self.api_key
+
+  def complete(self, prompt, max_tokens=150, temperature=0.8, top_p=1, frequency_penalty=0.2, stop=["\n", "\r"]):
+    logging.debug('prompt: %s', prompt)
+
+    if prompt is None:
+      return { "error": 'prompt is required'}
+    
+    response = openai.Completion.create(
+      model="curie:ft-personal-2023-06-09-09-05-53",
+      prompt=prompt,
+      max_tokens=max_tokens,
+      temperature=temperature,
+      top_p=top_p,
+      frequency_penalty=frequency_penalty,
+      stop=stop
+    )
+
+    return {
+      "message":response.choices[0].text.strip(),
+    }
