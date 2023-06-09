6129ea6b5a8dcfa7757a2efd0f155633775775fc
May Tusek <tusek.may@gmail.com>
2023-06-09 03:16:06 -0700

    Adds slight animation.

--- projects/app/src/components/Icon/Icon.tsx
+++ projects/app/src/components/Icon/Icon.tsx
@@ -7,9 +7,13 @@ interface IconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
 
 const StyledImage = styled("img")(() => ({
   borderRadius: "50%",
-  border: "2px solid #000000",
+  border: "4px solid #000000",
   boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25)",
   filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+  transition: "transform 0.2s",
+  "&:hover": {
+    transform: "rotate(45deg)",
+  },
 }))
 
 export const Icon: React.FC<IconProps> = ({ src, ...props }) => {
